b709f7a2036ccec2d7f96bfc163dbefe886c8904
Guy Israeli <guy.israeli@klarna.com>
2017-05-05 14:27:41 +0300

    rubocop

--- ekserciseapp/app/controllers/people_controller.rb
+++ ekserciseapp/app/controllers/people_controller.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 class PeopleController < ApplicationController
+  def index
+    page = params[:page] || 1
 
- def index
-  page = params[:page] || 1
+    people = User.page(page).per(50)
 
-  people = User.page(page).per(50)
-
-  render json: people
- end
-
+    render json: people
+  end
 end
--- ekserciseapp/config/routes.rb
+++ ekserciseapp/config/routes.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get :people, to: "people#index"
+  get :people, to: 'people#index'
 
-  root "application#root"
+  root 'application#root'
 end
