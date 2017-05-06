56bead6dea09fa2cd0f76b860001b65d85096ab4
Guy Israeli <guy.israeli@klarna.com>
2017-05-06 10:46:53 +0300

    add stuff

--- ekserciseapp/app/models/user.rb
+++ ekserciseapp/app/models/user.rb
@@ -1,6 +1,16 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  scope :by_name, ->(name) { where('name ILIKE ?', "%#{name}%") if name.present? }
+  scope :by_age, ->(age) {
+                   if age.present?
+                     where('birthday > :upper_limit AND birthday <= :bottom_limit',
+                           bottom_limit: age.to_i.years.ago.to_i,
+                           upper_limit:  (age.to_i + 1).years.ago.to_i)
+                   end
+                 }
+  scope :by_phone, ->(phone) { where('phone LIKE ?', "%#{phone}%") if phone.present? }
+
   def as_json(_)
     {
       'id'       => uid,
