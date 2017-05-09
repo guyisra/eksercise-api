6d628e640813cf2d089e20ce1b99e3f35b5357ac
Guy Israeli <guy.israeli@klarna.com>
2017-05-09 19:58:03 +0300

    fix cors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
+  after_action :cors_fix
+
   def root
     render 'application/swagger.html'
   end
 
   protected
+
+  def cors_fix
+    headers['Access-Control-Allow-Origin'] = '*'
+  end
 
   def redis
     Redis.current
