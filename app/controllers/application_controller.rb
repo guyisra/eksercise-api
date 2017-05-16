1a4f7003f32b8264a802779bf1d89ee201e684d3
Guy Israeli <guy.israeli@klarna.com>
2017-05-16 14:03:31 +0300

    keeps cors only in root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
-  after_action :cors_fix
+  after_action :cors_fix, only: :root
 
   def root
     render 'application/swagger.html'
